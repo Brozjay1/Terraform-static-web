927ed197c24ba6d1faf2cc7e41b09e1b628f84a3
Brozjay1 <afolabiabayomijohn@gmail.com>
2024-09-04 14:51:22 +0100

    re-adjusted file

--- init-main.tf
+++ init-main.tf
@@ -1,10 +1,10 @@
-terraform{
-    required_providers{
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.1"
-        }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.65.0"
     }
+  }
 }
 
 
